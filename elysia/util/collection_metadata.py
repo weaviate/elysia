c7f4af0ae33d5546f35714cef8a1f7211fede3f0
dannyjameswilliams <danny@weaviate.io>
2024-11-08 11:26:34 +0000

    fixed

--- elysia/util/collection_metadata.py
+++ elysia/util/collection_metadata.py
@@ -1,5 +1,6 @@
-
+import datetime
 from elysia.globals.weaviate_client import client
+from elysia.util.parsing import format_datetime
 
 def get_all_collection_names():
     return [k.lower() for k in list(client.collections.list_all().keys()) if k.lower().startswith("example_verba")]
@@ -8,7 +9,7 @@ def get_collection_data_types(collection_name: str):
     properties = client.collections.get(collection_name).config.get().properties
     return {property.name: property.data_type[:] for property in properties}
 
-def get_collection_data(collection_name: str, lower_bound: int = 0, upper_bound: int = -1):
+def get_collection_data(collection_name: str, lower_bound: int = 0, upper_bound: int = -1, convert_datetime: bool = True):
     collection = client.collections.get(collection_name)
 
     if upper_bound == -1:
@@ -17,5 +18,9 @@ def get_collection_data(collection_name: str, lower_bound: int = 0, upper_bound:
     items = []
     for i, item in enumerate(collection.iterator()):
         if i >= lower_bound and i < upper_bound:
+            if convert_datetime:
+                for key, value in item.properties.items():
+                    if isinstance(value, datetime.datetime):
+                        item.properties[key] = format_datetime(value)
             items.append(item.properties)
     return items
