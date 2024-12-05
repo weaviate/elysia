1bc07e684dcb5fd1dc821d03379a7949394c2196
dannyjameswilliams <danny@weaviate.io>
2024-12-05 16:16:07 +0000

    fixed conversation id issue + trees are independent

--- elysia/api/example.py
+++ elysia/api/example.py
@@ -4,9 +4,12 @@ import asyncio
 sys.path.append(os.getcwd())
 os.chdir("../..")
 
-from elysia.api.api_types import GetCollectionData, QueryData, GetCollectionsData
-from elysia.api.app import *
+from elysia.api.api_types import GetCollectionData, QueryData, GetCollectionsData, InitialiseTreeData
 from rich import print
+
+from elysia.api.routes.query import process
+from elysia.api.routes.tree import initialise_tree
+from elysia.api.services.tree import TreeManager
 
 import json
 
@@ -16,3 +19,31 @@ class fake_websocket:
         if data["type"] == "tree_update":
             print(f"connection from {data['payload']['node']} to {data['payload']['decision']}")
 
+tree_manager = TreeManager(
+    collection_names=[
+        "example_verba_github_issues", 
+        "example_verba_slack_conversations", 
+        "example_verba_email_chains", 
+    ]
+)
+
+async def main():
+
+    initialise_tree_data = InitialiseTreeData(
+        user_id="test",
+        conversation_id="test"
+    )
+
+    await initialise_tree(initialise_tree_data, tree_manager)
+
+    query_data = QueryData(
+        user_id="test",
+        conversation_id="test",
+        query="what was kaladin's most recent message?",
+        query_id="test"
+    )
+
+    await process(query_data.model_dump(), fake_websocket(), tree_manager)
+
+if __name__ == "__main__":
+    asyncio.run(main())
