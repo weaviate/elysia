09b38802385923e867668fc36b428fbf42716ae0
dannyjameswilliams <danny@weaviate.io>
2025-11-04 14:14:24 +0000

    updated package markers in __metadata__ and pyproject.toml

--- elysia/__metadata__.py
+++ elysia/__metadata__.py
@@ -1,5 +1,6 @@
 __name__ = "elysia"
-__version__ = "0.1.0.dev6"
+# replace_package_version_marker
+__version__ = "0.3.dev1"
 __description__ = "Elysia"
 __url__ = "https://github.com/weaviate/elysia"
 __author__ = "Danny Williams"
